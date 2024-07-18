79df04671d799b7cc065f743e1788969f02b6faf
Durga <mewadadurga83@gmail.com>
2024-07-18 12:16:19 +0530

    1.7

--- lib/Screen/1.7 Biometric Authentication/Provider/Biometric_Provider.dart
+++ lib/Screen/1.7 Biometric Authentication/Provider/Biometric_Provider.dart
@@ -7,33 +7,22 @@ import 'package:local_auth/local_auth.dart';
 class GalleryProvider extends ChangeNotifier{
 
   final LocalAuthentication auth = LocalAuthentication();
-  bool _didAuthenticate = false;
+  bool password = false;
 
   Future<void> localUserAuthentication(BuildContext context) async {
     List<BiometricType> availableBiometrics;
     try {
       availableBiometrics = await auth.getAvailableBiometrics();
-    } on PlatformException catch (e) {
-
-      return;
-    }
-
-    if (availableBiometrics.isEmpty) {
-
-      return;
-    }
-
+    } on PlatformException catch (e) {}
     try {
       final didAuthenticate = await auth.authenticate(
-        localizedReason: 'Please authenticate to access hide folder',
+        localizedReason: 'Verify with Fingerprint',
       );
-      _didAuthenticate = didAuthenticate;
+      password = didAuthenticate;
       notifyListeners();
-    } on PlatformException catch (e) {
+    } on PlatformException catch (e) {}
 
-    }
-
-    if (_didAuthenticate) {
+    if (password) {
       Navigator.of(context).push(MaterialPageRoute(
         builder: (context) => Safegallery(),
       ));
@@ -41,9 +30,5 @@ class GalleryProvider extends ChangeNotifier{
     }
   }
 
-  int randomNumber() {
-    Random random = Random();
-    int a = random.nextInt(1100);
-    return a;
-  }
+
 }
